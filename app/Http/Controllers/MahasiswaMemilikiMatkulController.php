ee6ff5100c0085bea87ee0a525c9353ba8b84006
chea <christorichard83@gmail.com>
2023-06-05 20:26:50 +0700

    Memberikan Class Controller dan Class

---
+++ app/Http/Controllers/MahasiswaMemilikiMatkulController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\MahasiswaMemilikiMatkul;
+use Illuminate\Http\Request;
+
+class MahasiswaMemilikiMatkulController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\MahasiswaMemilikiMatkul  $mahasiswaMemilikiMatkul
+     * @return \Illuminate\Http\Response
+     */
+    public function show(MahasiswaMemilikiMatkul $mahasiswaMemilikiMatkul)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\MahasiswaMemilikiMatkul  $mahasiswaMemilikiMatkul
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(MahasiswaMemilikiMatkul $mahasiswaMemilikiMatkul)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\MahasiswaMemilikiMatkul  $mahasiswaMemilikiMatkul
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, MahasiswaMemilikiMatkul $mahasiswaMemilikiMatkul)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\MahasiswaMemilikiMatkul  $mahasiswaMemilikiMatkul
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(MahasiswaMemilikiMatkul $mahasiswaMemilikiMatkul)
+    {
+        //
+    }
+}
