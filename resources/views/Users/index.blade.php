b5aac3f4528278f65322537c978c8071fa3e4c20
Tristanvivaldy <2172032@maranatha.ac.id>
2023-06-08 00:28:22 +0700

    Delete Register Page, insert user menu for admin

---
+++ resources/views/Users/index.blade.php
@@ -0,0 +1,63 @@
+@extends('layouts.master')
+
+@section('content')
+<!-- Content Header (Page header) -->
+<div class="content-header">
+    <div class="container-fluid">
+        <div class="row mb-2">
+            <div class="col-sm-6">
+                <h1 class="m-0 text-dark">User Management</h1>
+            </div><!-- /.col -->
+            <div class="col-sm-6">
+                <ol class="breadcrumb float-sm-right">
+                    <li class="breadcrumb-item"><a href="{{route('dashboard')}}">Dashboard</a></li>
+                    <li class="breadcrumb-item active">User Management</li>
+                </ol>
+            </div><!-- /.col -->
+        </div><!-- /.row -->
+    </div><!-- /.container-fluid -->
+</div>
+<!-- /.content-header -->
+
+<!-- Main content -->
+<div class="content">
+    <div class="container-fluid">
+        <div class="card">
+            <div class="card-header text-right">
+                <a href="{{ route('createUserList') }}" class="btn btn-primary" role="button">Add New User</a>
+            </div>
+            <div class="card-body p-0">
+                <table class="table table-hover mb-0 text-center">
+                    <thead>
+                        <tr>
+                            <th>ID</th>
+                            <th>Name</th>
+                            <th>Email</th>
+                            <th>Password</th>
+                            <th>Role</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach ($users as $user)
+                        <tr>
+                            <td>{{$user->id}}</td>
+                            <td>{{$user->name}}</td>
+                            <td>{{$user->email}}</td>
+                            <td>{{$user->password}}</td>
+                            <td>{{$user->role}}</td>
+                            <td>
+                                <a href="{{ route('EditUserList', ['id' => $user->id]) }}" class="btn btn-warning" role="button" style="cursor: pointer;"><i class="nav-icon fa fa-edit"></i></a>
+                                <a href="{{ route('deleteUser',['id'=>$user->id])}}" class="btn btn-danger" role="button" style="cursor: pointer; color: white;"><i class="nav-icon fa fa-trash"></i></a>
+                            </td>
+                        </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            </div>
+        </div>
+        {{-- main content here --}}
+
+    </div><!-- /.container-fluid -->
+</div>
+<!-- /.content -->
+@endsection
