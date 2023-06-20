dff0a0e65395432440d3cd12eb852b8b1ce96a32
JerryLivano <2172002@maranatha.ac.id>
2023-06-20 14:21:50 +0700

    tambah index dkbs

---
+++ resources/views/DKBS/index.blade.php
@@ -0,0 +1,64 @@
+@extends('layouts.master')
+
+@section('content')
+<!-- Content Header (Page header) -->
+<div class="content-header">
+    <div class="container-fluid">
+        <div class="row mb-2">
+            <div class="col-sm-6">
+                <h1 class="m-0 text-dark">Dokumen Kontrak Beban Studi</h1>
+            </div><!-- /.col -->
+            <div class="col-sm-6">
+                <ol class="breadcrumb float-sm-right">
+                    <li class="breadcrumb-item"><a href="{{route('dashboard')}}">Dashboard</a></li>
+                    <li class="breadcrumb-item active">DKBS</li>
+                </ol>
+            </div><!-- /.col -->
+        </div><!-- /.row -->
+    </div><!-- /.container-fluid -->
+</div>
+<!-- /.content-header -->
+
+<!-- Main content -->
+<div class="content">
+    <div class="container-fluid">
+        <div class="card">
+            <div class="card-body p-0">
+                <table class="table table-hover mb-0 text-center data">
+                    <thead>
+                    <tr>
+                        <th>Kode</th>
+                        <th>Mata Kuliah</th>
+                        <th>Kelas</th>
+                        <th>Hari</th>
+                        <th>Jam</th>
+                        <th>Ruangan</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                        @foreach($dkbss as $dkbs)
+                            <tr data-toggle="dropdown">
+                                <td>{{$dkbs->kode_matkul}}</td>
+                                <td>{{$dkbs->nama_matkul}}</td>
+                                <td>{{$dkbs->kelas}}</td>
+                                <td>{{$dkbs->hari}}</td>
+                                <td>{{\Carbon\Carbon::createFromFormat('H:i:s',$dkbs->jam_awal)->format('h:i')}} - {{\Carbon\Carbon::createFromFormat('H:i:s',$dkbs->jam_akhir)->format('h:i')}}</td>
+                                <td>{{$dkbs->ruangan}}</td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            </div>
+        </div>
+        {{-- main content here --}}
+
+    </div><!-- /.container-fluid -->
+</div>
+
+<script type="text/javascript">
+    $(document).ready(function () {
+        $('table').DataTable();
+    });
+</script>
+<!-- /.content -->
+@endsection
--- resources/views/mata_kuliah/edit.blade.php
+++ resources/views/mata_kuliah/edit.blade.php
@@ -25,7 +25,6 @@
 <!-- Main content -->
 <div class="content">
 	<div class="container-fluid">
-
 		<div class="card">
             <div class="card-body p-2">
                 <form action="{{ route('updateMataKuliah',['kode_matkul'=>$mataKuliah -> kode_matkul]) }}" method="post">
@@ -33,21 +32,23 @@
                     <div class="form-group">
                         <label for="txtKodeMatkul">Kode Mata Kuliah</label>
                         <input type="text" id="txtKodeMatkul" name="txtKodeMatkul" class="form-control" required placeholder="Kode Mata Kuliah" readonly value="{{ $mataKuliah->kode_matkul }}">
+                    </div>
+                    <div class="form-group">
                         <label for="txtName">Nama Mata Kuliah</label>
                         <input type="text" id="txtName" name="txtName" class="form-control" required placeholder="Nama Mata Kuliah" value="{{ $mataKuliah->nama_matkul }}">
+                    </div>
+                    <div class="form-group">
                         <label for="txtSemester">Semester</label>
                         <input type="text" id="txtSemester" name="txtSemester" class="form-control" required placeholder="Semester" value="{{ $mataKuliah->semester }}">
+                    </div>
+                    <div class="form-group">    
                         <label for="txtBebanSks">Beban SKS</label>
                         <input type="int" id="txtBebanSks" name="txtBebanSks" class="form-control" required placeholder="Beban SKS" value="{{ $mataKuliah->beban_sks }}">
+                    <div class="form-group">        
                         <label for="txtDeskripsi">Deskripsi</label>
-                        <input type="text" id="txtDeskripsi" name="txtDeskripsi" class="form-control" required placeholder="Deskripsi" value="{{ $mataKuliah->deskripsi }}">
-                        <!-- <label for="txtKodeProdi">Program Studi</label> -->
-                        <!-- <select name="txtKodeProdi" class="form-control">
-                            <option>Select Program Studi</option>
-                            @foreach ($prodi as $prodis)
-                                <option value="{{$prodis->kode_prodi}}">{{$prodis->nama_prodi}}</option>
-                            @endforeach
-                       </select> -->
+                        <textarea type="text" id="txtDeskripsi" name="txtDeskripsi" class="form-control">
+                            {{ $mataKuliah->deskripsi }}
+                        </textarea>
                     </div>
                     <div class="text-right">
                         <a href="{{ route('mataKuliahList') }}" class="btn btn-outline-secondary mr-2" role="button">Cancel</a>
